b56ceb2e978d9cf6fffd1e4a202ccf09949084ce
shoaib7262 <f2019108029@umt.edu.pk>
2022-11-18 15:01:16 +0500

    add funtionality to important compunents and delete item

--- src/App.js
+++ src/App.js
@@ -1,56 +1,74 @@
+import "./App.css";
 
-import './App.css';
-
-
-
-import SideNavbar from './My Components/SideNavbar';
+import SideNavbar from "./My Components/SideNavbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MyDay from './My Components/MyDay';
-import CommingTask from './My Components/ComingTask';
-import Important from './My Components/Important';
-import DoneTask from './My Components/DoneTask';
-import NewTask from './My Components/NewTask';
-import { createContext } from 'react';
-import { useState } from 'react';
+import MyDay from "./My Components/MyDay";
+import CommingTask from "./My Components/ComingTask";
+import Important from "./My Components/Important";
+import DoneTask from "./My Components/DoneTask";
+import NewTask from "./My Components/NewTask";
+import { createContext } from "react";
+import { useState } from "react";
 
 const allData = createContext();
 const daysData = createContext();
-
+const deletData = createContext();
+const importantData = createContext();
 
 function App() {
- const [tasks, setTasks] = useState([]);
-const addTasks =(data)=>{
-  setTasks([...tasks,data]);
-}
+  const [tasks, setTasks] = useState([]);
+  const addTasks = (data) => {
+    setTasks([...tasks, data]);
+  };
 
- 
+  const deletItem = (todoItem) => {
+    let todoList = tasks.filter((item) => item.id !== todoItem);
+    setTasks(todoList);
+  };
+
+  const importantIcon = (impItem) => {
+    let importantTask = tasks.map((item) => {
+      if (item.id === impItem) {
+        return {
+          ...item,
+          liked: !item.liked,
+        };
+      } else {
+        return item;
+      }
+    });
+    setTasks(importantTask);
+  };
+
   return (
     <>
-    <allData.Provider value={{addTasks}}>
-    <daysData.Provider value={tasks}>
-    <Router>
-        <div className="app">
-          
-          <SideNavbar />
+      <allData.Provider value={{ addTasks }}>
+        <daysData.Provider value={tasks}>
+          <deletData.Provider value={{ deletItem }}>
+            <importantData.Provider value={{ importantIcon }}>
+              <Router>
+                <div className="app">
+                  <SideNavbar />
 
-          <Routes>
-          <Route path="/" element={<MyDay />}  />
-          <Route path="/comingtask" element={<CommingTask />} />
-          <Route path="/important" element={<Important  />} />
-          <Route path="/donetask" element={<DoneTask />} />
-          <Route path="/addtask" element={<NewTask />} />
-
-
-        </Routes>
-
-        </div>
-    </Router>
-      </daysData.Provider>
-    </allData.Provider>
+                  <Routes>
+                    <Route path="/" element={<MyDay />} />
+                    <Route path="/comingtask" element={<CommingTask />} />
+                    <Route path="/important" element={<Important />} />
+                    <Route path="/donetask" element={<DoneTask />} />
+                    <Route path="/addtask" element={<NewTask />} />
+                  </Routes>
+                </div>
+              </Router>
+            </importantData.Provider>
+          </deletData.Provider>
+        </daysData.Provider>
+      </allData.Provider>
     </>
   );
 }
 
 export default App;
-export {allData};
-export {daysData};
+export { allData };
+export { daysData };
+export { deletData };
+export { importantData };
--- src/My Components/Important.js
+++ src/My Components/Important.js
@@ -1,13 +1,61 @@
-import React from 'react'
-import Header from './Header'
+import React, { useContext } from "react";
+import Header from "./Header";
+import { daysData } from "../App";
+import { deletData } from "../App";
 
 const Important = () => {
-  return (
-    <div className='important'>
-      <Header name= "Important"/>
-    
-    </div>
-  )
-}
+  const filterImportant = useContext(daysData);
+  const { deletItem } = useContext(deletData);
 
-export default Important
+  const onDelet = (id) => {
+    deletItem(id);
+  };
+
+  let impItems = filterImportant.filter((item) => item.liked === true);
+
+  return (
+    <div className="important">
+      <Header name="Important" />
+      {impItems.map((item) => {
+        return (
+          <div key={item.id}>
+            <div className="outer-card mx-2 my-2">
+              <div className="card mx-2">
+                <div className="card-body">
+                  <div className="dotedBorder">
+                    <div>
+                      <h6 className="card-title d-inline" id="card-title">
+                        {item.Title}
+                      </h6>
+                    </div>
+                    <div className="closeDiv">
+                      <i
+                        onClick={() => onDelet(item.id)}
+                        className="btn fa-solid fa-xmark"
+                      ></i>
+                    </div>
+                  </div>
+
+                  <p className="card-text" id="card-discription">
+                    {item.Discription}
+                  </p>
+                  <div className="iconDiv">
+                    <div className="inerDiv">
+                      <div>
+                        <button id="heartIcon">
+                          <i className="fa-solid fa-heart"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Important;
